cadf7d0f3e94cf729f9e6d7f8c7d4b049279da29
FN <firenamefn@gmail.com>
2025-08-03 22:01:52 +0700

    Comment

--- Content.Server/Administration/Commands/AdminWhoCommand.cs
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -7,7 +7,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Server.Administration.Commands;
 
-[AnyCommand]
+[AnyCommand] // Green-AdminWho
 public sealed class AdminWhoCommand : LocalizedCommands
 {
     [Dependency] private readonly IAfkManager _afkManager = default!;
