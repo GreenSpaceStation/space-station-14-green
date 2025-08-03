9bc65173e5b57600e913625ec9f384d61724dea8
FN <firenamefn@gmail.com>
2025-08-03 12:07:37 +0700

    Headset sound

---
+++ Content.Shared/_Green/Verbs/GreenVerbCategory.cs
@@ -0,0 +1,8 @@
+using Content.Shared.Verbs;
+
+namespace Content.Shared._Green.Verbs;
+
+public static class GreenVerbVategory
+{
+    public static readonly VerbCategory ToggleHeadsetSound = new("verb-categories-toggle-headset-sound", null);
+}
